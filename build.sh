363a910aecd7f3d7cf7a93bc1a3f1f4a84e575d8
Miguel Rojas <miguel_rojas@mentor.com>
2019-04-29 13:44:37 -0400

    We are refactoring the whole thing

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #lessc --verbose --clean-css="--s1 --advanced --compatibility=ie8" css_source/base.less css/style.css
-lessc --verbose css_source/base.less css/style.css
+lessc --verbose cssSource/base.less css/style.css
---
+++ js/tabs.js
@@ -0,0 +1,34 @@
+/**
+ * ProgUI Tabs component JS
+ * 
+ * @TODO: more than one tab element in the plugin
+ */
+(function($) {
+    /**
+     * Main constructor, builds tabs.
+     */
+    $.fn.ProgTabs = function(options) {
+        var defaults = {
+            activeTab: 0
+        };
+
+        var options = $.extend(defaults, options);
+
+        this.each(function(index, elem) {
+
+            $(this).find('div').hide()
+            $($(this).find('div')[options.activeTab]).show();
+            $(this).find('li').click(function(a,b){
+                $(this).parent().find("li").removeClass("active");
+                $(this).addClass("active");
+
+                var index = $(this).index();
+                var divs = $(this).parents(".tabs").find("div");
+                divs.hide();
+                $(divs[index]).show();
+            });
+
+            $($(this).find('li')[options.activeTab]).addClass("active");
+        });
+    };
+}(jQuery));
